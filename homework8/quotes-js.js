5c20fd05c8fa981b6f85442beca1a24d6db96fff
lj-shi <ljshi@andrew.cmu.edu>
2019-12-06 23:07:54 -0500

    quotes

---
+++ homework8/quotes-js.js
@@ -0,0 +1,21 @@
+
+function expandQuote(x) {
+	x.style.color = "#2d2c2c";
+	console.log("hello");
+}
+
+function hideQuote1(x) {
+	x.style.color = "#CEE1F7";
+}
+
+function hideQuote2(x) {
+	x.style.color = "#ACCCF2";
+}
+
+function hideQuote3(x) {
+	x.style.color = "#83A9DB";
+}
+
+function hideQuote4(x) {
+	x.style.color = "#4A90E2";
+}
